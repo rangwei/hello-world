9ca24a55cacf00de7166469f78382fed36136363
rangwei <winters.rang@sap.com>
2017-07-26 20:22:55 +0800

    yyyyy

---
+++ FoodTracker1/FoodTracker1Tests/FoodTracker1Tests.swift
@@ -0,0 +1,41 @@
+//
+//  FoodTracker1Tests.swift
+//  FoodTracker1Tests
+//
+//  Created by Rang, Winters on 25/06/2017.
+//  Copyright © 2017 Rang, Winters. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker1
+
+class FoodTracker1Tests: XCTestCase {
+    
+    //MARK: Meal Class Tests
+    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+    }
+    
+}
